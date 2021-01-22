720bc94df806328ec0bb32be46d7fc58b854e772
semanurtopal <77543320+semanurtopal@users.noreply.github.com>
2021-01-22 19:19:15 +0300

    DictionaryOdevi

---
+++ DictionaryOdev/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryOdev
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] _keys;
+        T2[] _values;
+
+            public MyDictionary()
+            {
+                _keys = new T1[0];
+                _values = new T2[0];
+            }
+            public void Add(T1 key, T2 value)
+            {
+            T1[] _tempArrayKeys = _keys;
+            T2[] _tempArrayValues = _values;
+
+            _keys = new T1[_keys.Length + 1];
+            _values = new T2[_values.Length + 1];
+
+                for (int i = 0; i < _tempArrayKeys.Length; i++)
+                {
+                _keys[i] = _tempArrayKeys[i];
+                _values[i] = _tempArrayValues[i];
+                }
+
+
+                _keys[_keys.Length - 1] = key;
+                _values[_values.Length - 1] = value;
+
+            }
+
+        public T1[] Keys
+        {
+            get { return _keys; }
+        }
+
+        public T2[] Values
+        {
+            get { return _values; }
+        }
+
+    }
+}
---
+++ DictionaryOdev/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DictionaryOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,string> myDictionary = new MyDictionary<string,string>();
+            myDictionary.Add("pencil","kalem");
+            myDictionary.Add("dress", "elbise");
+            myDictionary.Add("computer", "bilgisayar");
+
+
+
+            foreach (var kelime in myDictionary.Keys)
+            {
+                Console.WriteLine(kelime + ": ");
+                
+            }
+
+            foreach (var karşılık in myDictionary.Values)
+            {
+                Console.WriteLine(karşılık);
+            }
+
+        }
+    }
+}
